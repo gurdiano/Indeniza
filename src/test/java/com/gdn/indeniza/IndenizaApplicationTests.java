85409d4768d759bcce5475f3c3b134ebc57e3293
gurdiano <tiagofran2013@hotmail.com>
2023-10-31 10:37:00 -0300

    Project Created

---
+++ src/test/java/com/gdn/indeniza/IndenizaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdn.indeniza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndenizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
