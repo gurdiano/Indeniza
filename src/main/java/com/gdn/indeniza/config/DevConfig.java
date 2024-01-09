d586af5a15789fe9814a306eccff61e167b4a42f
gurdiano <tiagofran2013@hotmail.com>
2024-01-09 11:00:14 -0300

    Dev profile

---
+++ src/main/java/com/gdn/indeniza/config/DevConfig.java
@@ -0,0 +1,15 @@
+package com.gdn.indeniza.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig implements CommandLineRunner{
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
